2938231e1facf93707ba8872a0ec0b4eb5b060c7
SUJINJEONG012 <peekaboo32@naver.com>
2023-01-23 21:52:06 +0900

    JPA로 쇼핑몰 만들기

---
+++ src/main/java/com/shopping/study/ShoppingStudyApplication.java
@@ -0,0 +1,14 @@
+package com.shopping.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingStudyApplication.class, args);
+	}
+	
+
+}
---
+++ src/test/java/com/shopping/study/ShoppingStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
