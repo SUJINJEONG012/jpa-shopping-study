8cfdf711d4c0fd38e9414adfa59b656c3b2134f6
SUJINJEONG012 <peekaboo32@naver.com>
2023-01-25 00:10:56 +0900

    Querydsl 속성에 필요한 설정

--- src/main/java/com/shopping/study/repository/ItemRepository.java
+++ src/main/java/com/shopping/study/repository/ItemRepository.java
@@ -1,5 +1,6 @@
 package com.shopping.study.repository;
 
+
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,21 +9,28 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import com.shopping.study.entity.Item;
-//QuerydslPredicateExecutor<Item>
-public interface ItemRepository extends JpaRepository<Item, Long>
-{
- 
+
+
+public interface ItemRepository extends JpaRepository<Item, Long>,
+QuerydslPredicateExecutor<Item> {
+
+	List<Item> findByItemNm(String itemNm); // 상품명으로 데이터 조회
 	
-	List<Item> findByName(String itemNm); //상품명으로 데이터 조회
+	List<Item> findByItemNmOrItemDetail(String itemNm, String ItemDetail);//상품명 또는 상품상세설명을 조회
 	
-//	List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);// 상품명 또는 상품상세설명을 조회
-//	
-//	List<Item> findByPriceLessThan(Integer price);//파라미터로 넘어온 price변수보다 값이 작은 상품데이터 조회
-//	
-//	List<Item> findByPriceLessThanOrderByPriceDesc(Integer price); //상품의 가격이 높은 순으로 조회
-//	
-//	
-//	@Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc, nativeQuery =  true")
-//	List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+	List<Item> findByPriceLessThan(Integer price);//파라미터로 넘어온 price변수보다 값이 작은 상품데이터 조회
 	
+	List<Item> findByPriceLessThanOrderByPriceDesc(Integer price); //상품의 가격이 높은 순으로 조회
+
+	@Query("select i from Item i where i.itemDetail" 
+	+ " like " + 
+	"%:itemDetail% order by i.price desc")
+	List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+
 }
+
+
+
+
+
