fe523a33458ba6ed8d2b8aa37d7f62c0d9843f8b
SUJINJEONG012 <peekaboo32@naver.com>
2023-01-26 15:27:46 +0900

    상품데이터 dto생성해서 모델에담아 타임리프를 이용해 화면에 출력하기

--- src/main/java/com/shopping/study/controller/ThymeleafExController.java
+++ src/main/java/com/shopping/study/controller/ThymeleafExController.java
@@ -1,9 +1,13 @@
 package com.shopping.study.controller;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.shopping.study.dto.ItemDto;
 
 @Controller
 @RequestMapping(value="/thymeleaf")
@@ -13,6 +17,18 @@ public class ThymeleafExController {
 	public String thymeleafExample01(Model model) {
 		model.addAttribute("data", "타임리프 예제입니다.");
 		return "thymeleaf/thymeleaf01";
+	}
+	
+	@GetMapping(value="/ex02")
+	public String thymeleafExample02(Model model) {
+		ItemDto itemDto = new ItemDto();
+		itemDto.setItemNm("테스트 상품 1");
+		itemDto.setItemDetail("상품 상세 설명");
+		itemDto.setPrice(10000);
+		itemDto.setRegTime(LocalDateTime.now());
+		
+		model.addAttribute("itemDto", itemDto);
+		return "thymeleaf/thymeleaf02";
 	}
 
 }
