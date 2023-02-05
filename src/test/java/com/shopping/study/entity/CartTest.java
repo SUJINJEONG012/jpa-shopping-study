2f5569732cdc174c71ae14c2ec603feb95416dab
SUJINJEONG012 <peekaboo32@naver.com>
2023-02-05 22:29:59 +0900

    카트 테스트구현하기 콘솔창에 카트와 멤버정보도 같이 들고오는걸 확인하는거 구현, 여기서 즉시로딩이란 개념을 공부

--- src/test/java/com/shopping/study/entity/CartTest.java
+++ src/test/java/com/shopping/study/entity/CartTest.java
@@ -1,8 +1,66 @@
 package com.shopping.study.entity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.shopping.study.dto.UsersFormDto;
+import com.shopping.study.repository.CartRepository;
+import com.shopping.study.repository.UsersRepository;
+
 @SpringBootTest
 @Transactional
-@TestPropertySource()
+@TestPropertySource(locations="classpath:application-test.properties")
 public class CartTest {
+ 
+	@Autowired
+	CartRepository cartRepository;
+	
+	@Autowired
+	UsersRepository usersRepository;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	EntityManager em;
+	
+	
+	public Users createUsers() {
+		UsersFormDto usersFormDto = new UsersFormDto();
+		
+		usersFormDto.setEmail("angela@naver.com");
+		usersFormDto.setName("안젤");
+		usersFormDto.setAddress("대한민국");
+		usersFormDto.setPassword("1234");
+		return Users.createUser(usersFormDto, passwordEncoder);
+	}
+	
+	@Test
+	@DisplayName("장바구니 회원 엔티티 매핑 테스트 ")
+	public void findCartAndUsersTest() {
+		Users users = createUsers();
+		usersRepository.save(users);
+		
+		Cart cart = new Cart();
+		cart.setUsers(users);
+		cartRepository.save(cart);
+		
+		em.flush();
+		em.clear();
+		
+		
+		Cart savedCart = cartRepository.findById(cart.getId()).orElseThrow(EntityNotFoundException::new);
+		assertEquals(savedCart.getUsers().getId(), users.getId());
 
+	}
 }
